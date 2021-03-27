9702328227018980670614b1a306b1b229702f26
Matt Janda <matt.janda.kingston@gmail.com>
2021-03-27 19:43:19 +0000

    feat: Add turn exceptions

---
+++ src/RightTurn/Exceptions/TurnException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RightTurn.Exceptions
+{
+    public class TurnException : Exception
+    {
+        public TurnException(string message) : base(message)
+        {
+        }
+    }
+}
